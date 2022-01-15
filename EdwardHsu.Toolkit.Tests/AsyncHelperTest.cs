8b57978be5349a69dd55821124a169f09aef8a85
Edward Hsu <st89382000@gmail.com>
2022-01-16 02:36:45 +0800

    test: Add AsyncHelper unit test

---
+++ EdwardHsu.Toolkit.Tests/AsyncHelperTest.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EdwardHsu.Toolkit.Tests
+{
+    public class AsyncHelperTest
+    {
+
+        private void SampleV(ref int v)
+        {
+            Thread.Sleep(100);
+            v = 123;
+        }
+        private int SampleG()
+        {
+            Thread.Sleep(100);
+            return 123;
+        }
+
+        [Fact]
+        public async Task TaskG()
+        {
+            var value = await AsyncHelper.ToTask(() => SampleG());
+            Assert.Equal(value, 123);
+        }
+
+        [Fact]
+        public async Task TaskV()
+        {
+            int value = 0;
+            await AsyncHelper.ToTask(() => SampleV(ref value));
+            Assert.Equal(value, 123);
+        }
+    }
+}
