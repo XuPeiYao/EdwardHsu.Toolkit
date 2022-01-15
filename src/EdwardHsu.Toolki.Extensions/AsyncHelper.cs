4a6a5b3659cbb1dadb604e633818ca04330acaeb
Edward Hsu <st89382000@gmail.com>
2022-01-16 02:27:39 +0800

    feat: Add AsyncHelper

---
+++ src/EdwardHsu.Toolki.Extensions/AsyncHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EdwardHsu.Toolkit
+{
+    public static class AsyncHelper
+    {
+        public static Task ToTask(Expression<Action> expFunc)
+        {
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+
+            Task.Run(
+                () =>
+                {
+                    try
+                    {
+                        expFunc.Compile().Invoke();
+                        tcs.SetResult(null);
+                    }
+                    catch (Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                });
+
+            return tcs.Task;
+        }
+
+        public static Task<T> ToTask<T>(Expression<Func<T>> expFunc)
+        {
+            TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
+
+            Task.Run(
+                () =>
+                {
+                    try
+                    {
+                        var result = expFunc.Compile().Invoke();
+                        tcs.SetResult(result);
+                    }
+                    catch(Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                });
+
+            return tcs.Task;
+        }
+    }
+}
